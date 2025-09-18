3a2c18f30b7e39b50013d350f733d4670337bc76
feupam <feupam@gmail.com>
2025-09-18 08:29:08 -0300

    animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,10 +78,19 @@ const config: Config = {
             height: '0',
           },
         },
+        'scroll-left': {
+          '0%': {
+            transform: 'translateX(0)',
+          },
+          '100%': {
+            transform: 'translateX(-50%)',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'scroll-left': 'scroll-left linear infinite',
       },
     },
   },
